f7c5a22ce368d238361f0d39b9810b199b48af10
Giacomo212 <giacomo9812@gmail.com>
2023-12-28 23:06:49 +0100

    added logic project to repo

---
+++ Wireguard.net.ConfigAPI.Logic/ServerConfig/Abstractions/IServerConfigProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace Wireguard.net.ConfigAPI.Logic;
+
+public interface IServerConfigProvider {    
+
+    public string ServerPrivateKey { get; set; }
+    public string ServerPublicKey { get; set; }
+    public string ServerAddress { get; set; }
+    public int ServerPort { get; set; }
+}
---
+++ Wireguard.net.ConfigAPI.Logic/ServerConfig/Implementations/ServerConfigProvider.cs
@@ -0,0 +1,8 @@
+﻿namespace Wireguard.net.ConfigAPI.Logic;
+
+public class ServerConfigProvider : IServerConfigProvider {
+    public string ServerPrivateKey { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public string ServerPublicKey { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public string ServerAddress { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public int ServerPort { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+}
